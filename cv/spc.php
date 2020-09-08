9249b3094d714df5bf1ffa0b458c237801d9bc1e
Sergio Parada <sparada01@gmail.com>
2020-09-08 20:26:09 -0300

    Update spc.php

--- cv/spc.php
+++ cv/spc.php
@@ -1,5 +1,17 @@
 <?php
 
 // CV SPC
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CV Sergio Parada</title>
+	</head>
+	<body>
+    	<iframe src="https://drive.google.com/file/d/1p97dEUcIKT6KHNfR3QNe0_7ME9nXxpHZ/preview" width="100%" height="480"></iframe>
+	</body>
+</html>
+
+<?
 
 ?>
